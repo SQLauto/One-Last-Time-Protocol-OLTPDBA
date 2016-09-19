5fffaeaf8b36649ed10d9e8eebcca901eceb2220
g-kannan <g-kannan@outlook.com>
2016-09-19 11:59:00 +0530

    Script to get SQL Services state in Local/Remote host

---
+++ SCRIPTS/CGSV_IV.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+Gets computername as input and displays SQL Services. Also highlights stopped services in RED and running in GREEN
+#>
+[CmdletBinding()]
+param
+(
+[parameter(mandatory=$true)]
+[string]$server
+)
+
+
+Get-service  -ComputerName $server -displayname "*SQL*" | where{$_.StartType -ne "disabled"  } | select MachineName,status,name,displayname,StartType | foreach{
+if ($_.Status -eq 'Stopped')
+{[console]::ForegroundColor='red';$_;}
+else
+{[console]::ForegroundColor='Green';$_;}
+}
+
+[console]::ForegroundColor='White'
