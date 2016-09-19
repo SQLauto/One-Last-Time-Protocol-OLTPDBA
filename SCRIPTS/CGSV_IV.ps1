9cebda6dd13d72e9f5cdcb362754baf6beadb35f
g-kannan <g-kannan@outlook.com>
2016-09-19 12:21:28 +0530

    Updated script to accept multiple servernames & save output to PWD as CSV

--- SCRIPTS/CGSV_IV.ps1
+++ SCRIPTS/CGSV_IV.ps1
@@ -1,20 +1,33 @@
-﻿<#
-.Synopsis
-Gets computername as input and displays SQL Services. Also highlights stopped services in RED and running in GREEN
-#>
-[CmdletBinding()]
+﻿[CmdletBinding()]
 param
 (
 [parameter(mandatory=$true)]
-[string]$server
+[string[]]$server
 )
 
-
-Get-service  -ComputerName $server -displayname "*SQL*" | where{$_.StartType -ne "disabled"  } | select MachineName,status,name,displayname,StartType | foreach{
-if ($_.Status -eq 'Stopped')
-{[console]::ForegroundColor='red';$_;}
-else
-{[console]::ForegroundColor='Green';$_;}
+#Function from link: http://www.bgreco.net/powershell/format-color/
+function Format-Color([hashtable] $Colors = @{}, [switch] $SimpleMatch) {
+	$lines = ($input | Out-String) -replace "`r", "" -split "`n"
+	foreach($line in $lines) {
+		$color = ''
+		foreach($pattern in $Colors.Keys){
+			if(!$SimpleMatch -and $line -match $pattern) { $color = $Colors[$pattern] }
+			elseif ($SimpleMatch -and $line -like $pattern) { $color = $Colors[$pattern] }
+		}
+		if($color) {
+			Write-Host -ForegroundColor $color $line
+		} else {
+			Write-Host $line
+		}
+	}
 }
 
-[console]::ForegroundColor='White'
+$wdir = pwd | select -ExpandProperty path
+$Printdate = date -Format("ddMMMyyyy")
+
+Foreach ($PCname in $server)
+    {
+Get-service  -ComputerName $PCname -displayname "*SQL*" | where{$_.StartType -ne "disabled"  } | select MachineName,displayname,status,StartType | Format-Color @{'stopped' = 'red'} | ft -AutoSize
+Get-service  -ComputerName $PCname -displayname "*SQL*" | where{$_.StartType -ne "disabled"  } | select MachineName,displayname,status,StartType |Export-Csv "$wdir\SQLState_$printdate.csv"  -NoTypeInformation -force -append
+
+}
