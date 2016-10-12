dc1cf0c20f3d9fd32c70499b84e35450f0b0f10e
g-kannan <g-kannan@outlook.com>
2016-10-12 22:50:52 +0530

    CPU MEM

--- SQL_SCRIPTS/Daily.sql
+++ SQL_SCRIPTS/Daily.sql
@@ -27,3 +27,72 @@ xp_readerrorlog 0, 1, N'Logging SQL Server messages in file'
 
 --SQL Port in use
 select  distinct(local_tcp_port) from sys.dm_exec_connections
+
+
+--Current running queries sorted by CPU usage
+SELECT 
+	r.session_id
+	,st.TEXT AS batch_text
+	,SUBSTRING(st.TEXT, statement_start_offset / 2 + 1, (
+			(
+				CASE 
+					WHEN r.statement_end_offset = - 1
+						THEN (LEN(CONVERT(NVARCHAR(max), st.TEXT)) * 2)
+					ELSE r.statement_end_offset
+					END
+				) - r.statement_start_offset
+			) / 2 + 1) AS statement_text
+	,qp.query_plan AS 'XML Plan'
+	,r.*
+FROM sys.dm_exec_requests r
+CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) AS st
+CROSS APPLY sys.dm_exec_query_plan(r.plan_handle) AS qp
+ORDER BY cpu_time DESC
+
+--TOP CPU consumed query history
+select top 50 
+    sum(qs.total_worker_time) as total_cpu_time, 
+    sum(qs.execution_count) as total_execution_count,
+    count(*) as  number_of_statements, 
+    qs.plan_handle,
+	 est.text
+from 
+    sys.dm_exec_query_stats qs
+	cross apply sys.dm_exec_sql_text(qs.sql_handle) est
+group by qs.plan_handle,est.text
+order by sum(qs.total_worker_time) desc
+
+
+--Whats in buffer
+select type,name,sum(pages_kb/1024.0) as PagesMB
+from sys.dm_os_memory_clerks
+GROUP BY [type], [name]  
+ORDER BY SUM(pages_kb/1024.0) DESC
+
+--Memory state
+SELECT total_physical_memory_kb, available_physical_memory_kb, 
+       total_page_file_kb, available_page_file_kb, 
+       system_memory_state_desc
+FROM sys.dm_os_sys_memory;
+
+-- SQL Server Process Address space info (SQL 2008 and 2008 R2)
+--(shows whether locked pages is enabled, among other things)
+SELECT physical_memory_in_use_kb,locked_page_allocations_kb, 
+       page_fault_count, memory_utilization_percentage, 
+       available_commit_limit_kb, process_physical_memory_low, 
+       process_virtual_memory_low
+FROM sys.dm_os_process_memory;
+
+-- You want to see 0 for process_physical_memory_low
+-- You want to see 0 for process_virtual_memory_low
+
+-- Page Life Expectancy (PLE)
+SELECT cntr_value AS [Page Life Expectancy],counter_name,object_name
+FROM sys.dm_os_performance_counters
+WHERE
+ counter_name = N'Page life expectancy';
+
+-- PLE is a good measurement of memory pressure.
+-- Higher PLE is better. Below 300 is generally bad.
+-- Watch the trend, not the absolute value.
+
