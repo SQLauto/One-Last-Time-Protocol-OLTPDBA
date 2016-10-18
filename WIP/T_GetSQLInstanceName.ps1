20e62d3ed787e4fe997c022f9c5d735ad155ad6b
g-kannan <g-kannan@outlook.com>
2016-10-18 22:39:35 +0530

    Under Test

---
+++ WIP/T_GetSQLInstanceName.ps1
@@ -0,0 +1,20 @@
+﻿param(
+[cmdletbinding()]
+[parameter(mandatory=$true)]
+[string[]]$computername
+)
+
+foreach($server in $computername)
+{
+$namespace = gwmi -Namespace "root\microsoft\sqlserver" -Class "__Namespace" -Filter "name like 'ComputerManagement%'" -ComputerName $server | sort desc | select -ExpandProperty name
+
+ $raw_instance = GWMI -Namespace "root\microsoft\SqlServer\$namespace" -Class ServerSettings | select  -ExpandProperty instancename
+  
+  ForEach($iname in $raw_instance) 
+  {
+     if ($iname -eq "MSSQLSERVER") { $out_sqlins = "$server"; Write-Host "$out_sqlins" }
+     else  { $out_sqlins = ("$server\{0}" -f $iname); Write-Host "$out_sqlins"  };
+
+       }
+
+}
