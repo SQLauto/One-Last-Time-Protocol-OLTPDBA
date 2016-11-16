6624a9c4aeee55b8fc7d4aa4e4fe2007834865cb
g-kannan <g-kannan@outlook.com>
2016-11-16 20:13:59 +0530

    HasPWD

--- WIP/t_sendmailmessage.ps1
+++
@@ -1,5 +0,0 @@
-﻿$mcreden = New-Object System.Net.NetworkCredential($Username, $Password); 
-$Username = "oltpdba@gmail.com";
-$Password= "1800Oltpdba/";
-
-Send-MailMessage -SmtpServer "smtp.gmail.com" -Port 587 -UseSsl -From "Dr.Nefario <oltpdba@gmail.com>" -To "oltpdba@gmail.com" -Subject "SQL NOT running on preferred node" -Credential "oltpdba@gmail.com"
