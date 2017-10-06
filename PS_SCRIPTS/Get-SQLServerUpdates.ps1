0fec6bf517539d6e33e5a7ada98705018824327b
Kannan <Oltpdba@gmail.com>
2017-10-06 19:30:48 +0530

    OCT6 Commit

--- PS_SCRIPTS/Get-SQLServerUpdates.ps1
+++ PS_SCRIPTS/Get-SQLServerUpdates.ps1
@@ -1,4 +1,11 @@
 ﻿##Souce Links##
+#SQL Server update center - https://technet.microsoft.com/en-us/library/ff803383.aspx
+
+#2016
+$2016SP164 = "https://download.microsoft.com/download/3/0/D/30D3ECDD-AC0B-45B5-B8B9-C90E228BD3E5/ENU/SQLServer2016SP1-KB3182545-x64-ENU.exe"
+$2016CU5 = "https://download.microsoft.com/download/D/3/3/D33543E3-770F-4053-AE0F-A883696668FF/SQLServer2016-KB4040714-x64.exe"
+
+
 #2014
 $2014SP164 = "https://download.microsoft.com/download/2/F/8/2F8F7165-BB21-4D1E-B5D8-3BD3CE73C77D/SQLServer2014SP1-KB3058865-x64-ENU.exe"
 $2014SP132 = "https://download.microsoft.com/download/2/F/8/2F8F7165-BB21-4D1E-B5D8-3BD3CE73C77D/SQLServer2014SP1-KB3058865-x86-ENU.exe"
@@ -12,14 +19,9 @@ $2012SP264 ="https://download.microsoft.com/download/D/F/7/DF7BEBF9-AA4D-4CFE-B5
 $2012SP232 ="https://download.microsoft.com/download/D/F/7/DF7BEBF9-AA4D-4CFE-B5AE-5C9129D37EFD/SQLServer2012SP2-KB2958429-x86-ENU.exe"
 $2012SP364 ="https://download.microsoft.com/download/B/1/7/B17F8608-FA44-462D-A43B-00F94591540A/ENU/x64/SQLServer2012SP3-KB3072779-x64-ENU.exe"
 $2012SP332 ="https://download.microsoft.com/download/B/1/7/B17F8608-FA44-462D-A43B-00F94591540A/ENU/x86/SQLServer2012SP3-KB3072779-x86-ENU.exe"
+$2012SP464 = "https://download.microsoft.com/download/E/A/B/EABF1E75-54F0-42BB-B0EE-58E837B7A17F/SQLServer2012SP4-KB4018073-x64-ENU.exe"
+$2012SP432 = "https://download.microsoft.com/download/E/A/B/EABF1E75-54F0-42BB-B0EE-58E837B7A17F/SQLServer2012SP4-KB4018073-x86-ENU.exe"
 
-#2008 R2 - SP3
-$2K8R2SP364="https://download.microsoft.com/download/D/7/A/D7A28B6C-FCFE-4F70-A902-B109388E01E9/ENU/SQLServer2008R2SP3-KB2979597-x64-ENU.exe"
-$2K8R2SP332="https://download.microsoft.com/download/D/7/A/D7A28B6C-FCFE-4F70-A902-B109388E01E9/ENU/SQLServer2008R2SP3-KB2979597-x86-ENU.exe"
-
-#2008 - SP4
-$2K8SP464 = "https://download.microsoft.com/download/5/E/7/5E7A89F7-C013-4090-901E-1A0F86B6A94C/ENU/SQLServer2008SP4-KB2979596-x64-ENU.exe"
-$2K8SP432 = "https://download.microsoft.com/download/5/E/7/5E7A89F7-C013-4090-901E-1A0F86B6A94C/ENU/SQLServer2008SP4-KB2979596-x86-ENU.exe"
 
 ipmo bitstransfer
 New-Item -Path "C:\Users\$env:username\Desktop\SQLServicePack" -ItemType container -Force | Out-Null
@@ -32,21 +34,33 @@ $spn = Read-Host "
 Enter the number from below table to download respective SP
 --add '.2' to download 32 bit version 
 
+161 - SQL Server 2016 SP1
+1615 - SQL Server 2016 SP1 Latest Cumulative Update(CU5)
+
 141 - SQL Server 2014 SP1
 142 - SQL Server 2014 SP2
 
 121 - SQL Server 2012 SP1
 122 - SQL Server 2012 SP2
 123 - SQL Server 2012 SP3
+124 - SQL Server 2012 SP4
 
-103 - SQL Server 2008 R2 SP3
-
-104 - SQL Server 2008 SP4`n
 "
 
 	if ($spn -eq $null -or $spn -eq '' -or $spn -eq 'exit' ) 
 		{write-host -ForegroundColor yellow 'No selection made. Exiting.' 
 		BREAK}
+
+#2016 Download
+elseif ($spn -eq '161')
+		{Write-host "`n **Downloading SQL Server 2016 SP1 64 BIT** `n"
+        Start-BitsTransfer -Source $2016SP164 -Destination "C:\Users\$env:username\Desktop\SQLServicePack\SQLServer2016SP1-KB3182545-x64-ENU.exe"
+        }
+elseif ($spn -eq '1615')
+		{Write-host "`n **SQL Server 2016 SP1 Latest Cumulative Update(CU5)** `n"
+        Start-BitsTransfer -Source $2016CU5 -Destination "C:\Users\$env:username\Desktop\SQLServicePack\SQLServer2016-KB4040714-x64.exe"
+        }
+
 
 #2014 Download
 elseif ($spn -eq '141')
