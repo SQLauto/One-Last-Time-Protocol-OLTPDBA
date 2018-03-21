9210273f5b47e0ff91bcd3e9003c1c27826a1a81
Kannan <Oltpdba@gmail.com>
2018-03-21 22:34:23 +0530

    Summary added

--- PS_SCRIPTS/Push-PatchFiles.ps1
+++ PS_SCRIPTS/Push-PatchFiles.ps1
@@ -16,17 +16,27 @@ $FolderComment = 'SQLPatch'
 $SP  = 'K:\Test'
 $GDR = ''
 
+$PushOutcome=@()
+
 foreach ($ser in $Servername)
 {
 
 $BiggestDrive = gwmi win32_volume -ComputerName $ser -filter("drivetype = 3 and not label = 'System Reserved' and NOT Caption like '%\\VOLUME%'") | Sort-Object freespace -Descending | select -First 1 -ExpandProperty Caption
-
-Write-Host "Largest drive in $ser is $BiggestDrive" -ForegroundColor Yellow
+ 
+Write-Verbose "Largest drive in $ser is $BiggestDrive"
 $DestinationPath = '\\'+"$ser"+'\'+"$($BiggestDrive.replace(':','$'))"+$FolderComment+'\'
-Write-Host  $DestinationPath
+Write-Verbose  "using folder $DestinationPath to copy the files"
+
+$DestProp = New-Object System.Object
+$DestProp | Add-Member -type NoteProperty -name ServerName -Value $ser
+$DestProp | Add-Member -type NoteProperty -name PatchFiles_Path -Value $DestinationPath
+
+$PushOutcome += $DestProp
 
 cpi $sp  -Destination $DestinationPath -Recurse -Force
 #-Filter *.ZIP
 }
+
+$PushOutcome | ft -AutoSize
 
 }#Function Closure
