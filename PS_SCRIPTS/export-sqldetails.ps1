bad8cdace90133bb8f847f3cadf992e0389061c4
Kannan <Oltpdba@gmail.com>
2018-03-21 21:22:40 +0530

    Unwanted-rowsremoved

--- PS_SCRIPTS/export-sqldetails.ps1
+++ PS_SCRIPTS/export-sqldetails.ps1
@@ -14,10 +14,11 @@ foreach ($instance in $SQLInstance)
 {
 
 $Wd = "C:\Users\" ##add \ to the end
-$Filename= $Wd+$instance.ToUpper()+"_alldetails_"+ (Get-Date -Format "yyyy-MM-dd")+".xlsx"
+$Filename= $Wd+$instance.ToUpper()+"_alldetails_"+ (Get-Date -Format "yyyyMMddTHHmmss")+".xlsx"
 $Filename
 
-Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.databases" | Export-Excel -Path $Filename -WorkSheetname DBStatus -AutoSize -AutoFilter
-Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.configurations" | Export-Excel -Path $Filename -WorkSheetname configurations -AutoSize -AutoFilter
-Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.master_files" | Export-Excel -Path $Filename -WorkSheetname master_files -AutoSize -AutoFilter
+
+Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.databases" | Select-Object * -ExcludeProperty  "RowError","RowState","Table","ItemArray","HasErrors" | Export-Excel -Path $Filename -WorkSheetname DBStatus -AutoSize -AutoFilter
+Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.configurations" | Select-Object * -ExcludeProperty  "RowError","RowState","Table","ItemArray","HasErrors" |  Export-Excel -Path $Filename -WorkSheetname configurations -AutoSize -AutoFilter
+Invoke-Sqlcmd2 -ServerInstance $instance -Query "select * from sys.master_files" |  Select-Object * -ExcludeProperty  "RowError","RowState","Table","ItemArray","HasErrors" |  Export-Excel -Path $Filename -WorkSheetname master_files -AutoSize -AutoFilter
 }
