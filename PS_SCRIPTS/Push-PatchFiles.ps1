b476fc1d7ecf40ddf07cea5f331b0bed044a5f98
Kannan <Oltpdba@gmail.com>
2018-03-21 21:50:45 +0530

    Push-PatchFiles

---
+++ PS_SCRIPTS/Push-PatchFiles.ps1
@@ -0,0 +1,32 @@
+﻿function Push-PatchFiles
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string[]]$Servername
+    )
+
+$FolderComment = 'SQLPatch'
+$SP  = 'K:\Test'
+$GDR = ''
+
+foreach ($ser in $Servername)
+{
+
+$BiggestDrive = gwmi win32_volume -ComputerName $ser -filter("drivetype = 3 and not label = 'System Reserved' and NOT Caption like '%\\VOLUME%'") | Sort-Object freespace -Descending | select -First 1 -ExpandProperty Caption
+
+Write-Host "Largest drive in $ser is $BiggestDrive" -ForegroundColor Yellow
+$DestinationPath = '\\'+"$ser"+'\'+"$($BiggestDrive.replace(':','$'))"+$FolderComment+'\'
+Write-Host  $DestinationPath
+
+cpi $sp  -Destination $DestinationPath -Recurse -Force
+#-Filter *.ZIP
+}
+
+}#Function Closure
